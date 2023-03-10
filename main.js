5acaf47160a3d700658694c18af72b5ee9d05ce5
Ryan Engel <Rcengel@comcast.net>
2023-03-09 22:47:31 -0800

    innerHTML learned and render leads function added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* function saveLead() {
+/*function saveLead() {
     console.log("Button Clicked!")
 } */
 
@@ -7,17 +7,41 @@
 let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
+
 
 // use const, unless you know the value of the variable will be reassigned later on
 
-
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
+    myLeads.push(inputEl.value) //.value 
+    renderLeads()
 })
 
+function renderLeads() {
+let listItems = ""
+for (i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>" //innerHTML, manipulating the DOM using html elements
+}
+ulEl.innerHTML = listItems
+}
+
+//okay, the reason why we change the innerHTML to outside the loop is because 
+//it comes at a performance cost. Rather than having to manipulate the DOM three times (lthrough the loop)
+//we are defining the list item within js, then calling upon it once outside of the function
 
 
+
+
+// .innerHTML //
+
+/* const containerEl = document.querySelector("#container")
+// containerEl.innerHTML = "<button>" + "Buy!" + "</button>"
+or
+containerEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
+//on click is within the html value
+function buy() {
+    containerEl.innerHTML = "<p>Thank you for buying!</p>"
+} */
 
 
  
